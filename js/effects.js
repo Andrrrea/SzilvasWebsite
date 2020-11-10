0660f8883731f5921c5fcfa5608df1132c49bfd3
Andrea Engel <andreaengel@Andreas-MBP.box>
2020-11-10 21:41:19 +0100

    toggle menu on mobile

--- js/effects.js
+++ js/effects.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+  var navbar = document.getElementById("navbar5");
     $("a").on('click', function(event) {
       if (this.hash !== "") {
         event.preventDefault();
@@ -8,7 +9,19 @@ $(document).ready(function(){
           scrollTop: $(hash).offset().top
         }, 900, function(){
           window.location.hash = hash;
+          $(navbar).css("display","none");
         });
       } 
+    });
+  });
+
+  $(document).ready(function(){
+    var navbar = document.getElementById("navbar5");
+    var navbutton = document.getElementById("nav-button");
+
+    $(navbutton).on('click', function(event) {
+      if($(navbar).css('display') == 'none'){
+        $(navbar).css("display","inline");
+      }
     });
   });
